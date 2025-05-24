b598544b92a67e4fb1627a2df71878ababd45336
GonzaloMBustos <bustosgonzalom@gmail.com>
2025-05-24 17:31:59 -0300

    retry sql labo

--- LaboSQL_Optimization/GonzaloBustos.sql
+++ LaboSQL_Optimization/GonzaloBustos.sql
@@ -42,3 +42,37 @@ SELECT Name, CantidadAlbumes FROM AlbumCountByArtist WHERE CantidadAlbumes > 10 
 
 
 GO
+
+-- RETRY
+
+-- q1
+WITH TotalSpentByCustomer(FirstName, LastName, TotalSpent) AS (
+    SELECT FirstName, LastName, SUM(Total) as TotalSpent FROM Customer c
+    INNER JOIN Invoice i ON c.CustomerId = i.CustomerId
+    GROUP BY c.CustomerId, FirstName, LastName
+) SELECT FirstName, LastName, TotalSpent FROM TotalSpentByCustomer WHERE TotalSpent > (SELECT AVG(TotalSpent) FROM TotalSpentByCustomer) ORDER BY TotalSpent DESC;
+
+-- q2
+SELECT c.FirstName, c.LastName, COUNT(DISTINCT tr.GenreId) GenreCount FROM Customer c
+INNER JOIN Invoice i ON c.CustomerId = i.CustomerId
+INNER JOIN InvoiceLine il ON i.InvoiceId = il.InvoiceId
+INNER JOIN Track tr ON il.TrackId = tr.TrackId
+GROUP BY c.CustomerId, c.FirstName, c.LastName
+HAVING COUNT(DISTINCT tr.GenreId) > 1
+ORDER BY GenreCount DESC;
+
+-- q3
+
+SELECT al.Title FROM Album al
+INNER JOIN Track tr ON al.AlbumId = tr.AlbumId
+GROUP BY al.AlbumId, al.Title
+HAVING MIN(tr.Milliseconds) > (SELECT AVG(Milliseconds) FROM Track)
+ORDER BY al.Title ASC;
+
+
+-- q4
+SELECT ar.Name, COUNT(DISTINCT al.AlbumId) AlbumQuantity FROM Artist ar
+INNER JOIN Album al ON ar.ArtistId = al.ArtistId
+GROUP BY ar.ArtistId, ar.Name
+HAVING COUNT(DISTINCT al.AlbumId) > 10
+ORDER BY AlbumQuantity DESC;
